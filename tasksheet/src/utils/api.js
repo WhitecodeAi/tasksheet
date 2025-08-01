f6c1b9d3c6290a2992ae7f870b85fc010d1b67a6
Shruti Jog <shruti.jog@whitecode.co.in>
2025-08-01 16:00:15 +0530

    m

--- tasksheet/src/utils/api.js
+++ tasksheet/src/utils/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_API_BASE || 'http://localhost:3001';
-console.log('react app api base 1 - '+BASE_URL);
+ 
 export const api = axios.create({
   baseURL: BASE_URL,
   // Add headers or interceptors here if needed later
+ 
 });
