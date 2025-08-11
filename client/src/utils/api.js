c8f748fd12a4a4aefd4e5ba0ef00c6b6c7320e59
Shruti Jog <shruti.jog@gmail.com>
2025-08-11 10:55:55 +0530

    vite and CRA should work with one backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
 //const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
-const apiUrl = import.meta.env.VITE_API_URL;
 
+const apiUrl =
+  import.meta?.env?.VITE_API_URL || // Vite
+  process.env?.REACT_APP_API_URL || // CRA
+  'http://localhost:3001';          // Fallback for local dev
+
+  
 export const api = axios.create({
   baseURL: apiUrl,
   // Add headers or interceptors here if needed later
