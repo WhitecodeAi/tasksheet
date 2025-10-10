9bec0b1bf2c9402ef892afa5ccfd07fbd6a01cb5
Jaydeep <jaydeep.khelbude@whitecode.co.in>
2025-10-10 17:25:04 +0530

    F

--- server/db.js
+++ server/db.js
@@ -1,20 +1,14 @@
 const mysql = require('mysql2/promise');
 
-const requiredVars = ['DB_HOST', 'DB_USER', 'DB_PASSWORD', 'DB_NAME'];
 
-requiredVars.forEach((key) => {
-  if (process.env[key] === undefined) {
-    throw new Error(`Missing env var: ${key}`);
-  }
-});
  
  
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || '193.203.184.233',
   port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER || 'u207346834_WCadminAi',
+  password: process.env.DB_PASSWORD || 't8=Lp[/TxBH',
+  database: process.env.DB_NAME || 'u207346834_WCadminAi',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
