55d319a7ec606a4eccdcf175a19cbf42b7245406
Jaydeep <jaydeep.khelbude@whitecode.co.in>
2025-10-13 10:36:07 +0530

    Auto LogOUT

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -15,7 +15,14 @@ api.interceptors.request.use((config) => {
 
 api.interceptors.response.use(
   (res) => res,
-  (error) => Promise.reject(error)
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
 );
 
 export { api };
